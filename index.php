3b6854c95eb1f20af40853f6c6186bc8495e1ad0
EGbbit <elizabeth.githiri@strathmore.edu>
2025-09-06 19:41:00 +0300

    init code

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+class MyClass {
+    public function heading() {
+        echo "<h1>Welcome to BBIT DevOps</h1>";
+    }
+    public function myMethod() {
+        echo "This is a new semester";
+    }
+    public function footer() {
+        echo "<footer>Contact us at <a href='mailto:info@bbit.edu'>info@bbit.edu</a></footer>";
+    }
+}
+// Create an instance of the class
+$instance = new MyClass();
+
+// Call the method
+$instance->heading();
+$instance->myMethod();
+$instance->footer();
